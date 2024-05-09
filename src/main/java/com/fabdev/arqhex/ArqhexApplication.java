1f479ba3de4cd1ecc5e3d522ab08f0d2f69c9baf
fabmaiadev <fabxmaia@outlook.com>
2024-05-09 09:33:50 -0300

    commit 002 - add estrutura de pacotes do projeto e criando classes customer e adress no domain

---
+++ src/main/java/com/fabdev/arqhex/ArqhexApplication.java
@@ -0,0 +1,13 @@
+package com.fabdev.arqhex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArqhexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArqhexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabdev/arqhex/ArqhexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabdev.arqhex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArqhexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
