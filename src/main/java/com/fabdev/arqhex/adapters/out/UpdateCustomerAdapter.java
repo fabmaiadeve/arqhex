a1e46b18e0b5ae27ca9e3436c40788dc50ca46c6
fabmaiadev <fabxmaia@outlook.com>
2024-05-10 16:43:02 -0300

    commit 013 - criando o adapter de update do customer

---
+++ src/main/java/com/fabdev/arqhex/adapters/out/UpdateCustomerAdapter.java
@@ -0,0 +1,24 @@
+package com.fabdev.arqhex.adapters.out;
+
+import com.fabdev.arqhex.adapters.in.controller.mapper.CustomerMapper;
+import com.fabdev.arqhex.adapters.out.repository.CustomerRepository;
+import com.fabdev.arqhex.adapters.out.repository.mapper.CustomerEntityMapper;
+import com.fabdev.arqhex.application.core.domain.Customer;
+import com.fabdev.arqhex.application.ports.out.UpdateCustomerOutputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UpdateCustomerAdapter implements UpdateCustomerOutputPort {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerEntityMapper customerEntityMapper;
+    @Override
+    public void update(Customer customer) {
+        var customerEntity = customerEntityMapper.toCustomerEntity(customer);
+        customerRepository.save(customerEntity);
+    }
+}
