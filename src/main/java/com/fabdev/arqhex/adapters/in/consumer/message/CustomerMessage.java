a5f61824861b659623a8cc6f92bcba85338d45d8
fabmaiadev <fabxmaia@outlook.com>
2024-05-16 10:37:45 -0300

    commit 020 - ajuste para rodar a aplicacao local

--- src/main/java/com/fabdev/arqhex/adapters/in/consumer/message/CustomerMessage.java
+++ src/main/java/com/fabdev/arqhex/adapters/in/consumer/message/CustomerMessage.java
@@ -14,4 +14,44 @@ public class CustomerMessage {
     private String zipCode;
     private String cpf;
     private Boolean isValidCpf;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Boolean getIsValidCpf() {
+        return isValidCpf;
+    }
+
+    public void setIsValidCpf(Boolean isValidCpf) {
+        isValidCpf = isValidCpf;
+    }
 }
--- src/main/java/com/fabdev/arqhex/adapters/out/repository/entity/CustomerEntity.java
+++ src/main/java/com/fabdev/arqhex/adapters/out/repository/entity/CustomerEntity.java
@@ -18,4 +18,44 @@ public class CustomerEntity {
     private String cpf;
 
     private Boolean isValidCpf;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public AddressEntity getAddress() {
+        return address;
+    }
+
+    public void setAddress(AddressEntity address) {
+        this.address = address;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Boolean getIsValidCpf() {
+        return isValidCpf;
+    }
+
+    public void setIsValidCpf(Boolean isValidCpf) {
+        isValidCpf = isValidCpf;
+    }
 }
